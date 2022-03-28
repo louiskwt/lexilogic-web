294a83fe958ded981c6f2fed51809f9c2d8c39bb
Louis Tsang <kawingt2@gmail.com>
2022-03-28 22:35:31 +0800

    conntect to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log('Hello wordie')
+import express from "express";
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { connectDB }  from './config/db.js'
+
+dotenv.config()
+
+const port = process.env.PORT || 8000
+
+const app = express()
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+app.use(cors())
+
+
+
+connectDB()
+
+app.listen(port, () => console.log(`Sever started on port: ${[port]}`))
