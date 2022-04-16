95109b530225e76c66a4711f9ee5d97d09cc273e
Louis Tsang <kawingt2@gmail.com>
2022-04-16 11:50:40 +0800

    added stat and fixed modal style

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -7,6 +7,8 @@ const Modal = () => {
     const { gameState } = useGameState()
     const correctArr = gameState.correctGuess
     const wrongArr = gameState.wrongGuess
+    let total = wrongArr.length + correctArr.length
+    let corRate = Math.round(correctArr.length / total * 100)
  
   return (
     <div className='modal' style={{ 'display': modalState }} onClick={() => setModalState('none')}>
@@ -15,17 +17,27 @@ const Modal = () => {
                   <span className="close" onClick={() => setModalState('none')}>&times;</span>
                   <h1>我的記錄</h1>
             </div>
+            <div className='stat'>
+                <p>學習了 {total} 個詞彙</p>
+                {' '}
+                <p>正確率： {corRate} %</p>
+            </div>
             <div className="modal-body">
-                  {correctArr.length > 0 && correctArr.map((word, index) => (
-                      <p key={index}>
-                          {word} {' '} <span>✅</span>
-                      </p>
-                  ))}
-                  {wrongArr.length > 0 && wrongArr.map((word, index) => (
-                      <p key={index}>
-                          {word} {' '} <span>❌</span>
-                      </p>
-                  ))}
+                <div className='correct-list'>
+                      {correctArr.length > 0 && correctArr.map((word, index) => (
+                          <p key={index}>
+                              {word} {' '} <span>✅</span>
+                          </p>
+                      ))}
+                </div>
+                  <div className='wrong-list'>
+                      {wrongArr.length > 0 && wrongArr.map((word, index) => (
+                          <p key={index}>
+                              {word} {' '} <span>❌</span>
+                          </p>
+                      ))}
+                </div>
+         
             </div>
         </div>
     </div>
