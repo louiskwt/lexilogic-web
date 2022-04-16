e157b96779c01702580e3e0b91d93af519e95146
Louis Tsang <kawingt2@gmail.com>
2022-04-16 10:57:25 +0800

    fixed instruction style

--- frontend/src/components/Instructions/Instructions.jsx
+++ frontend/src/components/Instructions/Instructions.jsx
@@ -16,34 +16,42 @@ const Instructions = () => {
 
             <p>答案是一個歷年DSE出現過的詞彙。你可以按照螢幕上方的中文意思，去用下方的小鍵盤拼出答案，而你有6次機會去拼出正確答案✅，放膽去試啦～</p>
 
-            <p>提交答案時，按鍵盤中的 「 ENRER 」即可，如果你輸入了錯的字，可以按 「 del 」去刪除 </p>
+            <p>提交答案時，按鍵盤中的 「 ENRER 」即可，如果你輸入了錯的字，可以按 「 DEL 」去刪除 </p>
 
             <p>每次按「 ENRER 」後，方格會變成不同顏色去為你提供更多提示</p>
         </div>
         <hr />
         <div className='example'>
             <h2>例子</h2>
+            <div>
+                <p>答案： table</p>
+                  <p>答案入面有 E ，而且位置跟答案裡的 E 是一樣的，方格會變綠色👇🏼</p>
+            </div>
             <div className='tile-row'>
                 {firstEx.map((letter, index) => ( 
                    <GameTile key={index} letter={letter} state={(letter === 'e'? 'correct' : 'absent')} />
                 ))}
-                <p>答案： table</p>
             </div>
-            <p>答案入面有 E ，而且位置是正確的</p>
+
+            <div>
+                  <p>答案： easy</p>
+                  <p>輸入的字入面有 E，但位置跟答案裡的 E 不一樣，方格會變黃色👇🏼</p>
+            </div>
+  
               <div className='tile-row'>
                   {secondEx.map((letter, index) => (
                       <GameTile key={index} letter={letter} state={(letter === 'e' ? 'present' : 'absent')} />
                   ))}
-                  <p>答案： easy</p>
               </div>
-              <p>答案入面有 E ，但位置是不正確的</p>
+              <div>
+                  <p>答案： tummy</p>
+                  <p>上面輸入的字都不存在於答案，方格會變灰色👇🏼</p>
+              </div>
               <div className='tile-row'>
                   {thirdEx.map((letter, index) => (
                       <GameTile key={index} letter={letter} state={'absent'} />
                   ))}
-                  <p>答案： tummy</p>
               </div>
-              <p>輸入的字不存在於答案</p>
         </div>
         <hr />
         <div className='footer'>
