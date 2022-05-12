a986f574bf0b44ff3ab84955c999ee459efdd6bb
Louis Tsang <kawingt2@gmail.com>
2022-05-12 23:22:35 +0800

    upgrade to React 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
   
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('container')
-);
+
+const container = document.getElementById('container');
+const root = createRoot(container);
+
+root.render(<App />)
 
