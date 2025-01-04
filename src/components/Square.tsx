51e0a4b174ce9975c1fdf1b393bf4f76812ca104
Louis Tsang <kawingt2@gmail.com>
2025-01-04 22:58:06 +0800

    fixed square style

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -3,7 +3,7 @@ interface SquareProps {
 }
 
 const Square = ({char}: SquareProps) => {
-  return <div className="w-20 h-20 bg-gray-800 rounded-md shadow-md flex items-center justify-center text-white font-medium text-2xl">{char}</div>;
+  return <div className="w-20 h-20 bg-zinc-800 rounded-md border-zinc-500  border-2 shadow-md flex items-center justify-center text-white font-medium text-2xl">{char}</div>;
 };
 
 export default Square;
