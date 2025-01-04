9ff278d248456f50466e6a7c0548ae4856ec31b1
Louis Tsang <kawingt2@gmail.com>
2025-01-04 23:04:26 +0800

    rendered rows

--- src/games/Wordle.tsx
+++ src/games/Wordle.tsx
@@ -1,11 +1,23 @@
+import {useState} from "react";
 import Navbar from "../components/GameNav";
 import Keyboard from "../components/Keyboard";
+import Square from "../components/Square";
 
 const Wordle = () => {
+  const [rows, setRows] = useState<string[] | unknown[]>(Array.from({length: 6}).fill([" ", " ", " ", " ", " "]));
   return (
     <>
       <Navbar />
-      <h1>Wordle</h1>
+      <div className="flex-1 flex flex-col items-center justify-center space-y-4 mt-12">
+        {rows.map((row, index) => (
+          <div key={index} className="flex space-x-2">
+            {row.map((letter, i) => (
+              <Square key={i} char={letter} />
+            ))}
+          </div>
+        ))}
+      </div>
+
       <Keyboard />
     </>
   );
