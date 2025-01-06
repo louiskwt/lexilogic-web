19142ece476a85af24c1783ff3a39859c6b4352d
Louis Tsang <kawingt2@gmail.com>
2025-01-06 17:48:41 +0800

    fixed import case

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import {StrictMode} from "react";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter, Route, Routes} from "react-router";
 import App from "./App.tsx";
-import {WordleProvider} from "./contexts/WordleContext.tsx";
+import {WordleProvider} from "./contexts/wordleContext.tsx";
 import Wordle from "./games/Wordle.tsx";
 import "./index.css";
 
