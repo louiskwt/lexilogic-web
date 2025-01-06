f2f5db7b447fdf980bb2dad80f81d80bddee2458
Louis Tsang <kawingt2@gmail.com>
2025-01-06 17:10:16 +0800

    expanded keyboard to handle input

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,7 +1,14 @@
 import {faDeleteLeft} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {BaseSyntheticEvent} from "react";
 
-const Keyboard = () => {
+interface IKeyboardProps {
+  handleEnter: () => void;
+  handleBackSpace: () => void;
+  handleKeyPress: (key: string) => void;
+}
+
+const Keyboard = ({handleEnter, handleBackSpace, handleKeyPress}: IKeyboardProps) => {
   const topRow: string[] = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const middleRow: string[] = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const bottomRow: string[] = ["Back", "X", "Z", "C", "V", "B", "N", "M", "Enter"];
@@ -10,21 +17,38 @@ const Keyboard = () => {
       <div className="flex flex-col space-y-2">
         <div className="flex space-x-2">
           {topRow.map((key, index) => (
-            <button key={index} className={`bg-zinc-600 hover:bg-gray-700 text-white font-medium py-3 px-2 rounded-md flex-1`}>
+            <button
+              key={index}
+              className={`bg-zinc-600 hover:bg-gray-700 text-white font-medium py-3 px-2 rounded-md flex-1`}
+              onClick={(e: BaseSyntheticEvent) => {
+                handleKeyPress(e.target.innerText);
+              }}>
               {key}
             </button>
           ))}
         </div>
         <div className="flex space-x-2">
           {middleRow.map((key, index) => (
-            <button key={index} className={`bg-zinc-600 hover:bg-gray-700 text-white font-medium py-3 px-2 rounded-md flex-1`}>
+            <button
+              key={index}
+              className={`bg-zinc-600 hover:bg-gray-700 text-white font-medium py-3 px-2 rounded-md flex-1`}
+              onClick={(e: BaseSyntheticEvent) => {
+                handleKeyPress(e.target.innerText);
+              }}>
               {key}
             </button>
           ))}
         </div>
         <div className="flex space-x-2">
           {bottomRow.map((key, index) => (
-            <button key={index} className={`bg-zinc-600 hover:bg-gray-700 text-white font-medium py-3 px-2 rounded-md  ${key === "Back" || key === "Enter" ? "flex-1 flex-grow" : "flex-1"}`}>
+            <button
+              key={index}
+              className={`bg-zinc-600 hover:bg-gray-700 text-white font-medium py-3 px-2 rounded-md  ${key === "Back" || key === "Enter" ? "flex-1 flex-grow" : "flex-1"}`}
+              onClick={(e: BaseSyntheticEvent) => {
+                const innerText = e.target.innerText;
+                if (key !== "Back" && key !== "Enter") return handleKeyPress(innerText);
+                return key === "Back" ? handleBackSpace() : handleEnter();
+              }}>
               {key === "Back" ? <FontAwesomeIcon icon={faDeleteLeft} /> : key}
             </button>
           ))}
