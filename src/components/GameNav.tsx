76cb18856b299488015f7ff90e35cdfc627f9cea
Louis Tsang <kawingt2@gmail.com>
2025-01-06 23:49:57 +0800

    added game rule

--- src/components/GameNav.tsx
+++ src/components/GameNav.tsx
@@ -39,9 +39,12 @@ const Navbar = () => {
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
         {/* Modal content */}
         <h2 className="text-2xl font-bold mb-4">Game Rules</h2>
-        <p className="mb-4">{/* Add your game rules content here */}</p>
-        <h3 className="text-xl font-bold mb-2">Language</h3>
-        <div className="flex space-x-4">{/* Language selection buttons */}</div>
+        <p className="text-xl mb-4 font-semibold">
+          你要喺六次機會之內猜出隱藏嘅英文字。
+          <br /> <br />
+          每次輸入一個字後,系統會顯示唔同嘅顏色提示你點樣修正。綠色代表字母位置正確,黃色代表字母無錯但位置唔對,灰色就代表呢個字母完全唔係隱藏字。加油!
+        </p>
+        <p className="text-xl mb-4 font-semibold">快啲同朋友一齊玩啦!字謎遊戲每日都會推出一個新謎題,考考你嘅中文水平。既好玩又可以提升你嘅中文能力,仲唔快啲嚟試吓?</p>
       </Modal>
     </>
   );
