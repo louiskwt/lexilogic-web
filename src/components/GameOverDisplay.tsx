78c8e91399bc7681809ee3ebcb5eda3d0e804b29
Louis Tsang <kawingt2@gmail.com>
2025-01-07 18:08:22 +0800

    set up game over display

---
+++ src/components/GameOverDisplay.tsx
@@ -0,0 +1,19 @@
+interface IGameOverDisplayProps {
+  title: string;
+  message: string;
+  handleNewGame: () => void;
+}
+
+const GameOverDisplay = ({handleNewGame, title, message}: IGameOverDisplayProps) => {
+  return (
+    <>
+      <h2 className="text-2xl font-bold mb-4">{title}</h2>
+      <p className="mb-4">{message}</p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewGame}>
+        Start New Game
+      </button>
+    </>
+  );
+};
+
+export default GameOverDisplay;
