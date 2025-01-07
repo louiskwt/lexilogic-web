2de5d8be975b70149c1772ccaf483dec6d3b339c
Louis Tsang <kawingt2@gmail.com>
2025-01-07 15:03:16 +0800

    fixed square props

--- src/games/Wordle.tsx
+++ src/games/Wordle.tsx
@@ -5,14 +5,15 @@ import {useWordleContext} from "../contexts/wordleContext";
 
 const Wordle = () => {
   const {rows, handleKeyPress, handleBackspace, handleEnter} = useWordleContext();
+  console.log(rows);
   return (
     <>
       <Navbar />
       <div className="flex-1 flex flex-col items-center justify-center space-y-4 mt-12">
         {rows.map((row, index) => (
           <div key={index} className="flex space-x-2">
-            {row.map((letter, i) => (
-              <Square key={i} char={letter} />
+            {row.map((square, i) => (
+              <Square key={i} char={square.character} correct={square.correct} misplaced={square.misplaced} />
             ))}
           </div>
         ))}
