17e39865cf423262ab1a614f1d4b1346a93468de
Louis Tsang <kawingt2@gmail.com>
2025-01-07 14:31:42 +0800

    expanded square to handle answer checking and signal

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,9 +1,11 @@
 interface SquareProps {
   char: string;
+  correct: boolean;
+  misplaced: boolean;
 }
 
-const Square = ({char}: SquareProps) => {
-  return <div className="w-20 h-20 bg-zinc-800 rounded-md border-zinc-500  border-2 shadow-md flex items-center justify-center text-white font-medium text-2xl">{char}</div>;
+const Square = ({char, correct, misplaced}: SquareProps) => {
+  return <div className={`w-20 h-20 bg-zinc-800 rounded-md border-zinc-500  border-2 shadow-md flex items-center justify-center text-white font-medium text-2xl ${correct ? "bg-green-500 text-white" : misplaced ? "bg-yellow-500 text-white" : "bg-gray-700 text-white"}`}>{char}</div>;
 };
 
 export default Square;
