161bf7fd7cbdd80f0df263ea46b30a0e050a7c14
Louis Tsang <kawingt2@gmail.com>
2025-01-08 11:54:44 +0800

    changed modal button ui

--- src/components/GameOverDisplay.tsx
+++ src/components/GameOverDisplay.tsx
@@ -9,7 +9,7 @@ const GameOverDisplay = ({handleNewGame, title, message}: IGameOverDisplayProps)
     <>
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       <p className="mb-4">{message}</p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewGame}>
+      <button className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewGame}>
         Start New Game
       </button>
     </>
