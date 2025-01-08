2fce3d51c8a0bfae69a639975c486ed04aec5eb1
Louis Tsang <kawingt2@gmail.com>
2025-01-08 12:29:11 +0800

    passed wordhint props

--- src/games/Wordle.tsx
+++ src/games/Wordle.tsx
@@ -4,10 +4,10 @@ import Square from "../components/Square";
 import {useWordleContext} from "../contexts/WordleContext";
 
 const Wordle = () => {
-  const {rows, handleKeyPress, handleBackspace, handleEnter, misplacedLetters, correctLetters, wrongLetters} = useWordleContext();
+  const {rows, handleKeyPress, handleBackspace, handleEnter, misplacedLetters, correctLetters, wrongLetters, wordHint} = useWordleContext();
   return (
     <>
-      <Navbar />
+      <Navbar wordHint={wordHint} />
       <div className="flex-1 flex flex-col items-center justify-center space-y-4 mt-12">
         {rows.map((row, index) => (
           <div key={index} className="flex space-x-2">
