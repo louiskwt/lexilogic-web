53459ab617ec6ecb0fe1455b8459c75299b2432d
Louis Tsang <kawingt2@gmail.com>
2025-01-08 12:46:07 +0800

    scaffolded auth context

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,74 @@
+import {Session, User} from "@supabase/supabase-js";
+import {createContext, ReactNode, useEffect, useState} from "react";
+import supabase from "../supabaseClient";
+
+type AuthContextType = {
+  user: User | null;
+  session: Session | null;
+  isLoading: boolean;
+  signInWithGoogle: () => Promise<void>;
+  signInWithEmail: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  session: null,
+  isLoading: true,
+  signInWithGoogle: async () => {},
+  signInWithEmail: async (email, password) => {},
+  signOut: async () => {},
+});
+
+export const AuthProvider = ({children}: {children: ReactNode}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const session = supabase.auth.session();
+    setSession(session);
+    setUser(session?.user ?? null);
+    setIsLoading(false);
+
+    const {data: authListener} = supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+      setUser(session?.user ?? null);
+    });
+
+    return () => {
+      authListener?.subscription.unsubscribe();
+    };
+  }, []);
+
+  const signInWithGoogle = async () => {
+    await supabase.auth.signIn({
+      provider: "google",
+    });
+  };
+
+  const signInWithEmail = async (email: string, password: string) => {
+    await supabase.auth.signIn({
+      email,
+      password,
+    });
+  };
+
+  const signOut = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        session,
+        isLoading,
+        signInWithGoogle,
+        signInWithEmail,
+        signOut,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
