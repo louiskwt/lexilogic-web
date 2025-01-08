317c71b1424e7c7d3c238b3c70a0e37faa720088
Louis Tsang <kawingt2@gmail.com>
2025-01-08 12:57:54 +0800

    showed spinner in game when loading word

--- src/games/Wordle.tsx
+++ src/games/Wordle.tsx
@@ -1,23 +1,30 @@
 import Navbar from "../components/GameNav";
 import Keyboard from "../components/Keyboard";
+import Spinner from "../components/Spinner";
 import Square from "../components/Square";
 import {useWordleContext} from "../contexts/WordleContext";
 
 const Wordle = () => {
-  const {rows, handleKeyPress, handleBackspace, handleEnter, misplacedLetters, correctLetters, wrongLetters, wordHint} = useWordleContext();
+  const {rows, handleKeyPress, handleBackspace, handleEnter, misplacedLetters, correctLetters, wrongLetters, wordHint, isFetchingWord} = useWordleContext();
   return (
     <>
       <Navbar wordHint={wordHint} />
-      <div className="flex-1 flex flex-col items-center justify-center space-y-4 mt-12">
-        {rows.map((row, index) => (
-          <div key={index} className="flex space-x-2">
-            {row.map((square, index) => (
-              <Square key={index} char={square.character} misplaced={square.misplaced} correct={square.correct} />
+      {isFetchingWord ? (
+        <Spinner size="lg" />
+      ) : (
+        <>
+          <div className="flex-1 flex flex-col items-center justify-center space-y-4 mt-12">
+            {rows.map((row, index) => (
+              <div key={index} className="flex space-x-2">
+                {row.map((square, index) => (
+                  <Square key={index} char={square.character} misplaced={square.misplaced} correct={square.correct} />
+                ))}
+              </div>
             ))}
           </div>
-        ))}
-      </div>
-      <Keyboard handleKeyPress={handleKeyPress} handleEnter={handleEnter} handleBackSpace={handleBackspace} correctLetters={correctLetters} misplacedLetters={misplacedLetters} wrongLetters={wrongLetters} />
+          <Keyboard handleKeyPress={handleKeyPress} handleEnter={handleEnter} handleBackSpace={handleBackspace} correctLetters={correctLetters} misplacedLetters={misplacedLetters} wrongLetters={wrongLetters} />
+        </>
+      )}
     </>
   );
 };
