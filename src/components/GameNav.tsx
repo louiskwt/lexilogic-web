33090f82e20bb8149b29c2edcc24b855ed2ea3fd
Louis Tsang <kawingt2@gmail.com>
2025-01-08 12:35:50 +0800

    made hints ui nicer

--- src/components/GameNav.tsx
+++ src/components/GameNav.tsx
@@ -51,17 +51,22 @@ const Navbar = ({wordHint}: INavbarProps) => {
 
   const renderHints = (): ReactNode => (
     <>
-      <h3 className="text-xl font-bold mb-2">Hints</h3>
-      <p className="text-xl mb-4 font-semibold">
-        意思： {meaning} ({pos})
-      </p>
-      <hr />
-
-      <div className="flex space-x-4 mt-8 ">
-        <h4>Vowels</h4>
-        <p>
+      <h2 className="text-2xl font-bold mb-4">Hints</h2>
+      <div className="flex flex-col justify-between mb-4">
+        <p className="text-xl font-semibold">意思:</p>
+        <br />
+        <p className="text-xl">
+          <span className="text-white">{meaning}</span> ({pos})
+        </p>
+      </div>
+      <div className="border-t border-white pt-4">
+        <h3 className="text-xl font-bold mb-2">Vowels (A, E, I, O, U)</h3>
+        <p className="text-white text-lg font-bold">
           {vowels.map((v, key) => (
-            <span key={key}>{v}, </span>
+            <span key={key}>
+              {v.toUpperCase()}
+              {key < vowels.length - 1 ? ", " : ""}
+            </span>
           ))}
         </p>
       </div>
