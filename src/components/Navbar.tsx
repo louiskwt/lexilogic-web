fd6568021c2196b5da55b7818e1b4645027a9dc0
Louis Tsang <kawingt2@gmail.com>
2025-01-10 23:44:40 +0800

    fixed avatar showing when not logged in

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,24 +37,27 @@ const Navbar = () => {
   return (
     <>
       <nav className="flex justify-end items-center mb-8 px-4">
-        <div className="relative">
-          {profile?.avatar_url ? (
-            <div className="w-10 h-10 rounded-full bg-cover bg-center cursor-pointer" style={{backgroundImage: `url(${profile.avatar_url})`}} onClick={toggleDropdown} />
-          ) : (
-            <div className="w-10 h-10 rounded-full bg-lime-600 flex items-center justify-center text-white cursor-pointer" onClick={toggleDropdown}>
-              {profile?.username?.charAt(0).toUpperCase()}
-            </div>
-          )}
-          {showDropdown && (
-            <div className="absolute right-0 mt-2 w-48 bg-zinc-800 rounded-md shadow-lg z-10">
-              <div className="py-2">
-                <button className="block w-full text-left px-4 py-2 hover:bg-zinc-700 focus:outline-none" onClick={handleSignOut}>
-                  Sign Out
-                </button>
+        {profile && (
+          <div className="relative">
+            {profile?.avatar_url ? (
+              <div className="w-10 h-10 rounded-full bg-cover bg-center cursor-pointer" style={{backgroundImage: `url(${profile.avatar_url})`}} onClick={toggleDropdown} />
+            ) : (
+              <div className="w-10 h-10 rounded-full bg-lime-600 flex items-center justify-center text-white cursor-pointer" onClick={toggleDropdown}>
+                {profile?.username?.charAt(0).toUpperCase()}
               </div>
-            </div>
-          )}
-        </div>
+            )}
+            {showDropdown && (
+              <div className="absolute right-0 mt-2 w-48 bg-zinc-800 rounded-md shadow-lg z-10">
+                <div className="py-2">
+                  <button className="block w-full text-left px-4 py-2 hover:bg-zinc-700 focus:outline-none" onClick={handleSignOut}>
+                    Sign Out
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+
         {!profile && (
           <div>
             <button className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4 mr-2" onClick={() => openLoginModal()}>
