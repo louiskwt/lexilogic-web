d1574ae2c134e9e4842a9fd50a77234aebe97502
Louis Tsang <kawingt2@gmail.com>
2025-01-13 12:05:50 +0800

    made UI more responsive

--- src/games/Dictator.tsx
+++ src/games/Dictator.tsx
@@ -9,13 +9,13 @@ const DictatorGame: React.FC = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-8">Dictator</h1>
       {currentWord ? (
-        <div className="flex flex-col items-center">
-          <div className="text-4xl font-bold mb-4">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-4xl font-bold mb-4 flex justify-center">
             {currentWord.word.split("").map((_, index) => (
-              <input key={index} type="text" value={userInput[index] || ""} onChange={(e) => handleUserInput(e, index)} onKeyDown={(e) => handleDelete(e, index)} className={`bg-transparent border-b-2 mr-5 border-gray-400 focus:border-lime-600 focus:outline-none w-16 h-16 text-center text-4xl ${userInput[index] === currentWord.word[index].toLowerCase() ? "text-green-500" : "text-white"}`} />
+              <input key={index} type="text" value={userInput[index] || ""} onChange={(e) => handleUserInput(e, index)} onKeyDown={(e) => handleDelete(e, index)} className={`bg-transparent border-b-2 mr-2 md:mr-5 border-gray-400 focus:border-lime-600 focus:outline-none w-8 md:w-12 md:h-12 text-center text-4xl ${userInput[index] === currentWord.word[index].toLowerCase() ? "text-green-500" : "text-white"}`} />
             ))}
           </div>
-          <button onClick={playAudio} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4 flex items-center">
+          <button onClick={playAudio} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4 flex items-center mt-8">
             <FontAwesomeIcon icon={faVolumeUp} className="mr-2" />
             Play Audio
           </button>
