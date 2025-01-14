8bc71c47953f403f30bed1d91c66875bd7898a4f
Louis Tsang <kawingt2@gmail.com>
2025-01-14 11:42:28 +0800

    handle user input in ui

--- src/games/Dictator.tsx
+++ src/games/Dictator.tsx
@@ -12,8 +12,19 @@ const DictatorGame: React.FC = () => {
       {currentWord ? (
         <div className="flex flex-col items-center justify-center">
           <div className="text-4xl font-bold mb-4 flex justify-center">
-            {currentWord.word.split("").map((_, index) => (
-              <input key={index} ref={inputRefsArray[index]} type="text" value={userInput[index] || ""} maxLength={1} onClick={() => setCurrentIndex(index)} onChange={(e) => handleUserInput(e, index)} className={`bg-transparent border-b-2 mr-2 md:mr-5 border-gray-400 focus:border-lime-600 focus:outline-none w-8 md:w-12 md:h-12 text-center text-4xl ${userInput[index] === currentWord.word[index].toLowerCase() ? "text-green-500" : "text-white"}`} />
+            {userInput.map((val, index) => (
+              <input
+                key={index}
+                ref={inputRefsArray[index]}
+                type="text"
+                value={val}
+                maxLength={1}
+                onClick={() => setCurrentIndex(index)}
+                onChange={(e) => {
+                  handleUserInput(e, index);
+                }}
+                className={`bg-transparent border-b-2 mr-2 md:mr-5 border-gray-400 focus:border-lime-600 focus:outline-none w-8 md:w-12 md:h-12 text-center text-4xl ${userInput[index] === currentWord.word[index].toLowerCase() ? "text-green-500" : "text-white"}`}
+              />
             ))}
           </div>
           <button onClick={playAudio} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4 flex items-center mt-8">
