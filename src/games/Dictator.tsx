c77064f338a7b42ecc9db8acd3548b9a3ddfd70a
Louis Tsang <kawingt2@gmail.com>
2025-01-14 12:52:37 +0800

    improved UI flow

--- src/games/Dictator.tsx
+++ src/games/Dictator.tsx
@@ -9,7 +9,6 @@ const DictatorGame: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-8">Dictator</h1>
-
       {currentWord ? (
         <div className="flex flex-col items-center justify-center">
           <div className="border-b-2 flex justify-items-center space-x-2">
@@ -17,7 +16,7 @@ const DictatorGame: React.FC = () => {
               <FontAwesomeIcon icon={faHeart} key={index} size="lg" color="lime" />
             ))}
           </div>
-          <div className="text-4xl font-bold mb-4 flex justify-center">
+          <div className="text-4xl font-bold mb-4 flex justify-center mt-6">
             {userInput.map(({character, correct}, index) => (
               <input
                 key={index}
@@ -29,22 +28,33 @@ const DictatorGame: React.FC = () => {
                 onChange={(e) => {
                   handleUserInput(e, index);
                 }}
-                className={`bg-transparent border-b-2 mr-2 md:mr-5 border-gray-400 focus:border-lime-600 focus:outline-none w-8 md:w-12 md:h-12 text-center text-4xl ${correct ? "text-green-500" : "text-white"}`}
+                className={`bg-transparent border-b-2 mr-2 md:mr-5 border-gray-400 focus:border-lime-600 focus:outline-none w-8 md:w-12 md:h-12 text-center text-4xl ${correct ? "text-green-500" : tries === 5 || character === "" ? "text-white" : "text-red-500"}`}
               />
             ))}
           </div>
-          <button onClick={playAudio} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4 flex items-center mt-8">
-            <FontAwesomeIcon icon={faVolumeUp} className="mr-2" />
-            Play Audio
-          </button>
-          {userInput.every((i) => i.character !== "") && (
+          {!isCorrect && (
+            <button onClick={playAudio} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4 flex items-center mt-8">
+              <FontAwesomeIcon icon={faVolumeUp} className="mr-2" />
+              Play Audio
+            </button>
+          )}
+
+          {!isCorrect && userInput.every((i) => i.character !== "") && (
             <button onClick={checkAns} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4 flex items-center mt-8">
               <FontAwesomeIcon icon={faCheck} className="mr-2" />
               Check Answer
             </button>
           )}
 
-          {isCorrect && <div className="text-green-500 font-bold mt-4">Correct!</div>}
+          {isCorrect && (
+            <>
+              {" "}
+              <div className="text-green-500 font-bold mt-4">Correct!</div>{" "}
+              <button onClick={startGame} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4">
+                Next Challenge
+              </button>
+            </>
+          )}
         </div>
       ) : (
         <button onClick={startGame} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4">
