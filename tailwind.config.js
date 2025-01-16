5282d8f2b932f56a975f508f68a0243fe5502aa2
Louis Tsang <kawingt2@gmail.com>
2025-01-16 15:54:55 +0800

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "drop-bounce": "drop 1s ease-out, bounce 0.6s ease-out",
+      },
+      keyframes: {
+        drop: {
+          "0%": {transform: "translateY(-100%)"},
+          "100%": {transform: "translateY(0)"},
+        },
+        bounce: {
+          "0%, 100%": {transform: "translateY(0)"},
+          "50%": {transform: "translateY(-20%)"},
+        },
+      },
+    },
   },
   plugins: [],
 };
