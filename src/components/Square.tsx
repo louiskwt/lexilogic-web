b5d1f45f5ebed99eac046222a040a86a58e7c2cd
Louis Tsang <kawingt2@gmail.com>
2025-01-17 14:29:46 +0800

    shrinked square on small screen

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -6,7 +6,7 @@ interface SquareProps {
 
 const Square = ({char, correct, misplaced}: SquareProps) => {
   return (
-    <div className={`w-20 h-20 relative group`}>
+    <div className={"w-10 h-10 md:w-20 md:h-20 relative group"}>
       <div className={`absolute inset-0 bg-zinc-800 rounded-md border-zinc-500 border-2 shadow-md flex items-center justify-center text-white font-medium text-3xl transition-transform duration-1000 transform-style preserve-3d ${correct ? "rotate-y" : misplaced ? "rotate-y" : ""}`}>
         <div className={`absolute inset-0 backface-hidden flex items-center justify-center ${correct ? "bg-green-500 text-white" : misplaced ? "bg-yellow-500 text-white" : "bg-gray-700 text-white"}`}>{char}</div>
         <div className="absolute inset-0 flex items-center justify-center">{char}</div>
