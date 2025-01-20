b169cecf6ce93b3486026b22612ae93e7ae00266
Louis Tsang <kawingt2@gmail.com>
2025-01-20 19:45:38 +0800

    fixed gameover display ui

--- src/components/GameOverDisplay.tsx
+++ src/components/GameOverDisplay.tsx
@@ -13,7 +13,7 @@ const GameOverDisplay = ({handleNewGame, title, message, answer = ""}: IGameOver
     <>
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       <p className="mb-4">{message}</p>
-      <p className="m-4">{t("correctAnswer", {ans: answer})}</p>
+      <p className="mb-4">{t("correctAnswer", {ans: answer})}</p>
       <button className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewGame}>
         {t("tryAgain")}
       </button>
