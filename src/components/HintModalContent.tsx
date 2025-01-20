44b67ad059e44219d5a1d0323dfa3306e89ea969
Louis Tsang <kawingt2@gmail.com>
2025-01-20 19:22:29 +0800

    used i18n in hint modal

--- src/components/HintModalContent.tsx
+++ src/components/HintModalContent.tsx
@@ -1,3 +1,5 @@
+import {useLanguageContext} from "../contexts/LanguageContext";
+
 interface IHingModalContentProps {
   meaning: string;
   pos: string;
@@ -5,18 +7,19 @@ interface IHingModalContentProps {
 }
 
 const HintModalContent = ({meaning, pos, vowels}: IHingModalContentProps) => {
+  const {t} = useLanguageContext();
   return (
     <>
-      <h2 className="text-2xl font-bold mb-4">Hints</h2>
+      <h2 className="text-2xl font-bold mb-4">{t("hintModal.title")}</h2>
       <div className="flex flex-col justify-between mb-4">
-        <p className="text-xl font-semibold">意思:</p>
+        <p className="text-xl font-semibold">{t("hintModal.meaning")}:</p>
         <br />
         <p className="text-xl">
           <span className="text-white">{meaning}</span> ({pos})
         </p>
       </div>
       <div className="border-t border-white pt-4">
-        <h3 className="text-xl font-bold mb-2">Vowels (A, E, I, O, U)</h3>
+        <h3 className="text-xl font-bold mb-2">{t("hintModal.vowels")}</h3>
         <p className="text-white text-lg font-bold">
           {vowels.map((v, key) => (
             <span key={key}>
