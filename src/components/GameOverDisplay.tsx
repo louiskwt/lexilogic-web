fa36d3bab80d028f1ad690097567309f04b9f866
Louis Tsang <kawingt2@gmail.com>
2025-01-20 15:13:09 +0800

    showed correct answer in gameover display and use translated copy

--- src/components/GameOverDisplay.tsx
+++ src/components/GameOverDisplay.tsx
@@ -1,16 +1,21 @@
+import {useLanguageContext} from "../contexts/LanguageContext";
+
 interface IGameOverDisplayProps {
   title: string;
   message: string;
+  answer: string;
   handleNewGame: () => void;
 }
 
-const GameOverDisplay = ({handleNewGame, title, message}: IGameOverDisplayProps) => {
+const GameOverDisplay = ({handleNewGame, title, message, answer = ""}: IGameOverDisplayProps) => {
+  const {t} = useLanguageContext();
   return (
     <>
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       <p className="mb-4">{message}</p>
+      <p className="m-4">{t("correctAnswer", {ans: answer})}</p>
       <button className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewGame}>
-        Start New Game
+        {t("tryAgain")}
       </button>
     </>
   );
