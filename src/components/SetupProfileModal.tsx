986340acea37b15d66a74ed43c2e967402a4bfe9
Louis Tsang <kawingt2@gmail.com>
2025-01-21 12:19:33 +0800

    added copies for set up profile modal and required user input to be filled out

--- src/components/SetupProfileModal.tsx
+++ src/components/SetupProfileModal.tsx
@@ -1,5 +1,6 @@
 import {User} from "@supabase/supabase-js";
 import {useState} from "react";
+import {useLanguageContext} from "../contexts/LanguageContext";
 import supabase from "../supabaseClient";
 import Modal from "./Modal";
 
@@ -12,8 +13,14 @@ interface SetupProfileModalProps {
 const SetupProfileModal: React.FC<SetupProfileModalProps> = ({user, isOpen, onClose}) => {
   const [username, setUsername] = useState("");
   const [avatar, setAvatar] = useState<File | null>(null);
+  const {t} = useLanguageContext();
 
   const handleSubmit = async () => {
+    if (username === "") {
+      alert(t("warning.missingUsername"));
+      return;
+    }
+
     if (user) {
       try {
         const {data, error} = await supabase.from("profiles").insert([
@@ -56,17 +63,17 @@ const SetupProfileModal: React.FC<SetupProfileModalProps> = ({user, isOpen, onCl
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="bg-zinc-800 rounded-lg p-6 shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold mb-4">Set Up Your Profile</h2>
+        <h2 className="text-2xl font-bold mb-4">{t("setUpProfile")}</h2>
         <div className="flex flex-col space-y-4">
           <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} className="bg-zinc-700 rounded-md border-2 border-zinc-600 py-2 px-4 focus:outline-none focus:border-lime-600" />
           <div>
             <label className="block text-gray-400 mb-2" htmlFor="avatar">
-              Avatar (optional)
+              {t("avatar")}
             </label>
             <input type="file" id="avatar" onChange={(e) => setAvatar(e.target.files?.[0] || null)} className="bg-zinc-700 rounded-md border-2 border-zinc-600 py-2 px-4 focus:outline-none focus:border-lime-600" />
           </div>
           <button onClick={handleSubmit} className="bg-lime-600 hover:bg-lime-50 hover:text-gray-800 rounded-md border-2 text-white font-bold py-2 px-4">
-            Save Profile
+            {t("saveProfile")}
           </button>
         </div>
       </div>
