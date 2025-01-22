871b910c6bf210257bbfcad62e2981c8c111a5f1
Louis Tsang <kawingt2@gmail.com>
2025-01-22 23:41:35 +0800

    added types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type WordData = {
+  word: string;
+  pos: string;
+  audio: string;
+  meaning: string;
+};
+
+export type PhraseData = {
+  phrase: string;
+  pos: string;
+  meaning: string;
+  audio: string;
+};
