b7392fff65f77df60229953ba5debb861708bf5d
Louis Tsang <kawingt2@gmail.com>
2025-01-24 09:23:58 +0800

    added pos and meaning for wordle game over display

--- src/components/GameOverDisplay.tsx
+++ src/components/GameOverDisplay.tsx
@@ -4,16 +4,18 @@ interface IGameOverDisplayProps {
   title: string;
   message: string;
   answer: string;
+  pos: string;
+  meaning: string;
   handleNewGame: () => void;
 }
 
-const GameOverDisplay = ({handleNewGame, title, message, answer = ""}: IGameOverDisplayProps) => {
+const GameOverDisplay = ({handleNewGame, title, message, answer = "", pos = "", meaning = ""}: IGameOverDisplayProps) => {
   const {t} = useLanguageContext();
   return (
     <>
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       <p className="mb-4">{message}</p>
-      <p className="mb-4">{t("correctAnswer", {ans: answer})}</p>
+      <p className="mb-4">{t("correctAnswer", {ans: answer, pos, meaning})}</p>
       <button className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewGame}>
         {t("tryAgain")}
       </button>
