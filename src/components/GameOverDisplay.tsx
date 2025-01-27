8d7912a793c395c0cf12b39ac98d0443cd47835a
Louis Tsang <kawingt2@gmail.com>
2025-01-27 12:07:18 +0800

    fixed correct ans display escape value

--- src/components/GameOverDisplay.tsx
+++ src/components/GameOverDisplay.tsx
@@ -15,7 +15,16 @@ const GameOverDisplay = ({handleNewGame, title, message, answer = "", pos = "", 
     <>
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       <p className="mb-4">{message}</p>
-      <p className="mb-4">{t("correctAnswer", {ans: answer, pos, meaning})}</p>
+      <p className="mb-4">
+        {t("correctAnswer", {
+          ans: answer,
+          pos,
+          meaning,
+          interpolation: {
+            escapeValue: false,
+          },
+        })}
+      </p>
       <button className="bg-lime-600 hover:bg-lime-700 text-white font-bold py-2 px-4 rounded" onClick={handleNewGame}>
         {t("tryAgain")}
       </button>
