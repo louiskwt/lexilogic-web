fdc96f9446430da91534b0f10f0bc23ec41b15f9
Louis Tsang <kawingt2@gmail.com>
2025-01-29 16:28:02 +0800

    refactored and updated error page UI

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,8 +1,11 @@
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>Error</h1>
-      <p>Something went wrong. Please try again later.</p>
+    <div className="flex flex-col items-center justify-center h-screen bg-zinc-800 text-white">
+      <div className="flex flex-col items-center">
+        <span className="text-6xl mb-4">😟</span>
+        <h1 className="text-4xl font-bold mb-2">⚠️Error⚠️</h1>
+        <p className="text-xl">🚧 Something went wrong. Please try again later. 🚧</p>
+      </div>
     </div>
   );
 };
