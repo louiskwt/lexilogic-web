b8167687f8931513b7704b84c7556fd1b3bf7ebf
Louis Tsang <kawingt2@gmail.com>
2025-01-29 16:41:14 +0800

    added chinese copy

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,10 +1,11 @@
 const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-zinc-800 text-white">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center text-center">
         <span className="text-6xl mb-4">😟</span>
         <h1 className="text-4xl font-bold mb-2">⚠️Error⚠️</h1>
-        <p className="text-xl">🚧 Something went wrong. Please try again later. 🚧</p>
+        <p className="text-xl mb-3">🚧 Something went wrong. Please try again later. 🚧</p>
+        <p className="text-xl">🚧 哎呀,看起來出了點小問題。請等下再試一次吧 🚧</p>
       </div>
     </div>
   );
