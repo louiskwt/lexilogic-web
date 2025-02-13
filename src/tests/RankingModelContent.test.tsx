cf2c0a5772057bd10f8da4bf8d0be06883765a33
Louis Tsang <kawingt2@gmail.com>
2025-02-13 13:17:50 +0800

    update rankiing model content test to render properly but mock still doesn't work

---
+++ src/tests/RankingModelContent.test.tsx
@@ -0,0 +1,73 @@
+import {render, screen} from "@testing-library/react";
+import {MemoryRouter} from "react-router";
+import {beforeEach, describe, expect, it, vi} from "vitest";
+import RankingModalContent from "../components/RankingModalContent.tsx";
+import {AuthProvider} from "../contexts/AuthContext";
+import {LanguageProvider} from "../contexts/LanguageContext";
+import "../i18n.ts";
+
+describe("Ranking Model Content", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should render Ranking Model Content properly and prompt user to login when not logged in", () => {
+    render(
+      <MemoryRouter>
+        <LanguageProvider>
+          <AuthProvider>
+            <RankingModalContent />
+          </AuthProvider>
+        </LanguageProvider>
+      </MemoryRouter>
+    );
+
+    // Assert that the expected elements are rendered
+    expect(screen.getAllByText("Please log in to view the rankings")).toBeDefined();
+    expect(screen.getAllByText("Log In")).toBeDefined();
+  });
+
+  //   it("should render Ranking Model Content and display user info properly", () => {
+  //     vi.mocked(useAuthContext).mockReturnValue({
+  //       user: null,
+  //       profile: {
+  //         avatar_url: "url",
+  //         full_name: "testing",
+  //         id: "21234",
+  //         updated_at: "2023-12-23",
+  //         username: "testinguser",
+  //         website: "",
+  //         weekly_xp: 123,
+  //         total_xp: 1223,
+  //         meaning_lang: "en",
+  //       },
+  //       session: null,
+  //       isLoading: false,
+  //       signInWithGoogle: vi.fn(),
+  //       signInWithEmail: vi.fn(),
+  //       signOut: vi.fn(),
+  //       closeModal: vi.fn(),
+  //       showModal: false,
+  //       openLoginModal: vi.fn(),
+  //       openSignUpModal: vi.fn(),
+  //       toggleModal: vi.fn(),
+  //       signUpWithEmail: vi.fn(),
+  //       isProfileSetUpModalOpen: false,
+  //       setIsProfileModalOpen: vi.fn(),
+  //       handleProfileUpdate: vi.fn(),
+  //     });
+
+  //     render(
+  //       <MemoryRouter>
+  //         <LanguageProvider>
+  //           <AuthProvider>
+  //             <RankingModalContent />
+  //           </AuthProvider>
+  //         </LanguageProvider>
+  //       </MemoryRouter>
+  //     );
+
+  //     // Assert that the expected elements are rendered
+  //     expect(screen.getAllByText("Hi testinguser")).toBeDefined();
+  //   });
+});
