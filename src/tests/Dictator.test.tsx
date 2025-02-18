e6b54799e1405e455bd430a85bed5734ebb45ed7
Louis Tsang <kawingt2@gmail.com>
2025-02-18 10:07:01 +0800

    test Dictator Start Game and Game Over Display

---
+++ src/tests/Dictator.test.tsx
@@ -0,0 +1,75 @@
+import {IUserInput, IWord, WordHint} from "@/types/index.ts";
+import {cleanup, render, screen} from "@testing-library/react";
+import {MemoryRouter} from "react-router";
+import {afterEach, beforeEach, describe, expect, it, vi} from "vitest";
+import {AuthProvider} from "../contexts/AuthContext.tsx";
+import {DictatorContext, DictatorProvider} from "../contexts/DictatorContext";
+import {LanguageProvider} from "../contexts/LanguageContext";
+import Dictator from "../games/Dictator";
+import "../i18n.ts";
+
+describe("DictatorGame", () => {
+  const mockCurrentWord: IWord = {word: "cat", audio: "cat"};
+  const mockUserInput: IUserInput[] = Array.from({length: 3}).map(() => {
+    return {character: "", correct: false};
+  });
+  const mockWordHint: WordHint = {pos: "noun", meaning: "a fluffy animal", vowels: ["a"]};
+  const DictatorCtxValues = {
+    currentWord: mockCurrentWord,
+    userInput: mockUserInput,
+    isCorrect: false,
+    currentIndex: 0,
+    inputRefsArray: [],
+    tries: 5,
+    isGameOver: false,
+    wordHint: mockWordHint,
+    isFetchingWord: false,
+    startGame: () => {},
+    handleUserInput: () => {},
+    playAudio: () => {},
+    setCurrentIndex: () => {},
+    checkAns: () => {},
+  };
+
+  beforeEach(() => {
+    vi.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+    cleanup();
+  });
+
+  it("should render the start game page when not fetching word and not game over", () => {
+    render(
+      <MemoryRouter>
+        <LanguageProvider>
+          <AuthProvider>
+            <DictatorProvider>
+              <Dictator />
+            </DictatorProvider>
+          </AuthProvider>
+        </LanguageProvider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getAllByText("Start Game")).toBeDefined();
+  });
+
+  it("should render the game over page when game is over", () => {
+    render(
+      <MemoryRouter>
+        <LanguageProvider>
+          <AuthProvider>
+            <DictatorContext.Provider value={{...DictatorCtxValues, isGameOver: true}}>
+              <Dictator />
+            </DictatorContext.Provider>
+          </AuthProvider>
+        </LanguageProvider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getAllByText("Oops!")).toBeDefined();
+    expect(screen.getAllByText("Try Again")).toBeDefined();
+  });
+});
