271b68974cc760d252a68507d7aee6d0e09fa13a
Louis Tsang <kawingt2@gmail.com>
2025-02-18 09:04:22 +0800

    move component test to test folder

--- src/tests/Square.test.tsx
+++ src/tests/Square.test.tsx
@@ -1,6 +1,6 @@
 import {render, screen} from "@testing-library/react";
 import {describe, expect, it} from "vitest";
-import Square from "./Square";
+import Square from "../components/Square";
 
 describe("Square", () => {
   it("should render the correct character", () => {
