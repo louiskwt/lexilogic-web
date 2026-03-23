ac4d6ab76fe000410764c11113603183f542f848
Louis Tsang <kawingt2@gmail.com>
2026-03-23 16:27:16 +0800

    set up pickword by frequency and getword function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
-import {LangaugeOptions, LearnedWordPayload, LocalPhrases, LocalWords, PhraseData, ProfileData, WordData} from "@/types";
-import supabase from "../supabaseClient";
+import {LangaugeOptions, LocalPhrases, PhraseData, PickedWord, ProfileData, WordData} from "@/types";
+import PublicGoogleSheetsParser from "public-google-sheets-parser";
 
 export function findVowels(word: string): string[] {
   if (!word) return [];
@@ -42,27 +42,67 @@ export function isDateOneDayBefore(currentDate: Date, referenceDate: Date) {
   return timeDiffInMilliseconds >= oneDayInMilliseconds;
 }
 
-export async function updateXP(profileId: string, weeklyXP: number, totalXP: number) {
-  const {error} = await supabase.from("profiles").update({weekly_xp: weeklyXP, total_xp: totalXP}).eq("id", profileId);
+// export async function updateXP(profileId: string, weeklyXP: number, totalXP: number) {
+//   const {error} = await supabase.from("profiles").update({weekly_xp: weeklyXP, total_xp: totalXP}).eq("id", profileId);
 
-  if (error) {
-    setLocalProfileData({
-      weekly_xp: weeklyXP,
-      total_xp: totalXP,
-      date: new Date(),
-      meaning_lang: "zh",
-    });
+//   if (error) {
+//     setLocalProfileData({
+//       weekly_xp: weeklyXP,
+//       total_xp: totalXP,
+//       date: new Date(),
+//       meaning_lang: "zh",
+//     });
+//   }
+// }
+
+const LEXI_WORDS = "LEXI_WORDS";
+
+export function getWord(length = 5, currentFrequency = Infinity, usedWords = new Set()): Promise<PickedWord> | null {
+  try {
+    const parser = new PublicGoogleSheetsParser("16FdDn6cew8nIrl70IQl-GyLfYMFsL1AOpAjMr1nf3rE");
+    const word = parser
+      .parse()
+      .then((data: WordData[]) => {
+        const wordData = pickWord(data, length, currentFrequency, usedWords);
+        return wordData;
+      })
+      .catch((error) => {
+        console.log(error);
+        return {picked: null, nextFrequency: Infinity, usedWords: new Set()};
+      });
+
+    return word;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
 }
 
-export function getLocalWords(format: "dictationWords" | "wordleWords"): LocalWords | null {
-  const words = localStorage.getItem(format);
+export function pickWord(words: WordData[], length: number = 5, currentFrequency: number = Infinity, usedWords = new Set(), poolRatio = 0.2): PickedWord {
+  const candidates = words.filter((w) => w.word.length === length && w.frequency <= currentFrequency && !usedWords.has(w)).sort((a, b) => b.frequency - a.frequency);
+
+  if (candidates.length === 0) return {picked: null, nextFrequency: 1, usedWords};
+
+  const poolSize = Math.max(1, Math.ceil(candidates.length * poolRatio));
+  const pool = candidates.slice(0, poolSize);
+  const picked = pool[Math.floor(Math.random() * pool.length)];
+  usedWords.add(picked.word);
+
+  return {
+    picked,
+    nextFrequency: picked.frequency, // feed this back in on the next call
+    usedWords,
+  };
+}
+
+export function getLocalWords(): WordData[] | null {
+  const words = localStorage.getItem(LEXI_WORDS);
   if (!words) return null;
   return JSON.parse(words);
 }
 
-export function storeLocalWords(words: WordData[], format: string, date: Date | string = new Date()) {
-  localStorage.setItem(format, JSON.stringify({words, createdAt: date}));
+export function storeLocalWords(words: WordData[]) {
+  localStorage.setItem(LEXI_WORDS, JSON.stringify(words));
 }
 
 export function getLocalPhrases(): LocalPhrases | null {
@@ -94,16 +134,16 @@ export function getMeaningLangPreference(): LangaugeOptions {
   return (localStorage.getItem("meaning_lang") as LangaugeOptions) || getDefaultLangPreference();
 }
 
-export async function upsertLearnedWords(payload: LearnedWordPayload) {
-  try {
-    if (payload.word_id) {
-      const {error} = await supabase.from("learned_words").upsert(payload).select();
-      if (error) throw error;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-}
+// export async function upsertLearnedWords(payload: LearnedWordPayload) {
+//   try {
+//     if (payload.word_id) {
+//       const {error} = await supabase.from("learned_words").upsert(payload).select();
+//       if (error) throw error;
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
 export function truncateString(s: string, max: number | null = null): string {
   if (!max) return s;
